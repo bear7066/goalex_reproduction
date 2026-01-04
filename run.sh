98a3453f06ab9f3d9b511b17448f438437d7c2a0
bear <kidflash7011@gmail.com>
2026-01-04 22:38:10 +0800

    feat: modify script

--- run.sh
+++ run.sh
@@ -5,18 +5,22 @@ fi
 
 DATASET=$1
 
-python src/iterative_cluster.py \
-  --data_path processed_data/$DATASET \
-  --exp_dir experiments/$DATASET \
-  --proposer_model gpt-oss-20b \
-  --assigner_name gpt-oss-20b \
-  --proposer_num_descriptions_to_propose 30 \
-  --assigner_for_final_assignment_template templates/t5_multi_assigner_one_output.txt \
-  --iterative_max_rounds 2 \
-  --verbose
 
+LOG_FILE="experiments/$DATASET/log_$(date +%Y%m%d_%H%M%S).txt"
+echo "Logging to $LOG_FILE"
 
+{
+  python src/iterative_cluster.py \
+    --data_path processed_data/$DATASET \
+    --exp_dir experiments/$DATASET \
+    --proposer_model gpt-oss-20b \
+    --assigner_name gpt-oss-20b \
+    --proposer_num_descriptions_to_propose 30 \
+    --assigner_for_final_assignment_template templates/t5_multi_assigner_one_output.txt \
+    --iterative_max_rounds 2 \
+    --verbose
 
-python src/evaluate_only.py \
-  --data_path processed_data/$DATASET \
-  --exp_dir experiments/score/$DATASET
+  python src/evaluate_only.py \
+    --data_path processed_data/$DATASET \
+    --exp_dir experiments/$DATASET
+} 2>&1 | tee "$LOG_FILE"
